642fc709c8854a46569ee5b41e2f6b1d86657c24
Pibotics3814 <pibotics3814@gmail.com>
2022-01-24 18:24:07 -0500

    Luke: shooter/intake framework

--- Rapid_React_2022/src/main/java/frc/robot/RobotContainer.java
+++ Rapid_React_2022/src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,8 @@ package frc.robot;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import frc.robot.commands.ExampleCommand;
+import frc.robot.subsystems.*;
+import frc.robot.commands.*;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -21,6 +23,8 @@ public class RobotContainer {
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   private final ExampleCommand m_autoCommand = new ExampleCommand(m_exampleSubsystem);
+
+  public final DriveTrain m_piboticsdrive = new DriveTrain();
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
